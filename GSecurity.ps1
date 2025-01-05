9da2c6959d316641601261c4eced04ad4eddd672
Gorstak1979 <znastidobrostoje@gmail.com>
2025-01-05 02:46:54 +0100

    Add files via upload

--- GSecurity.ps1
+++ GSecurity.ps1
@@ -3,7 +3,8 @@
     Author: Gorstak
     Description: Advanced script to detect and mitigate web servers, screen overlays, keyloggers, suspicious DLLs, remote thread execution, and unauthorized files.
                  Monitors all local drives and network shares, ensures critical services are running, and protects critical system processes and specific trusted drivers from termination.
-                 Runs invisibly without disrupting the calling batch file.
+                 Runs invisibly without disrupting the calling batch file. While it is not designed to be antivirus replacement, it aims to be 2nd layer of defense for high profile targets.
+                 It is a part of larger script suite, called GShield with other scripts offering gaming tweaks and additional tweaks and policies for hardening.
     Version: 3.0
     License: Free for personal use
 #>
